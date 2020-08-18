fbf91e6687f951cbf15e5b754a1da03a961b5279
HotwheelH <haiderkhan343@gmail.com>
2020-08-18 15:37:31 +0100

    populated bean on toast recipe page and created a page for poached eggs. updated some string values too

---
+++ app/src/main/java/com/example/recipeapp/PoachedEggs.java
@@ -0,0 +1,14 @@
+package com.example.recipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PoachedEggs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poached_eggs);
+    }
+}
